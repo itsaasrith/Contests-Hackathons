6ed4cfca456a381affed03359f7ae19c0c395404
Deepak_Kumar <dkctcak@gmail.com>
2023-12-17 20:08:15 +0530

    Adds component LeafletMap to the UserDashboard

--- IndiaInternationalScienceFestivalISRO/app1/src/pages/UserDashboard.tsx
+++ IndiaInternationalScienceFestivalISRO/app1/src/pages/UserDashboard.tsx
@@ -1,20 +1,80 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React from "react";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/react";
+import LeafletMap from "../components/LeafletMap";
 
 const UserDashboard: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="light">
+          <IonTitle className="ion-padding-left">Dashboard</IonTitle>
+        </IonToolbar>
+      </IonHeader>
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>User Dashboard</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
-            </IonContent>
-        </IonPage>
-    );
+      {/* <IonGrid>
+        <IonRow>
+          <IonCol size="6">
+            <IonCard color="light">
+              <IonCardHeader>
+                <IonCardSubtitle>Card 1</IonCardSubtitle>
+                <IonCardTitle>Feature 1</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>This is the feature.</IonCardContent>
+            </IonCard>
+          </IonCol>
+
+          <IonCol size="6">
+            <IonCard color="light">
+              <IonCardHeader>
+                <IonCardSubtitle>Card 2</IonCardSubtitle>
+                <IonCardTitle>Feature 2</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>This is the feature.</IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+
+        <IonRow>
+          <IonCol size="6">
+            <IonCard color="light">
+              <IonCardHeader>
+                <IonCardSubtitle>Card 3</IonCardSubtitle>
+                <IonCardTitle>Feature 3</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>This is the feature.</IonCardContent>
+            </IonCard>
+          </IonCol>
+
+          <IonCol size="6">
+            <IonCard color="light">
+              <IonCardHeader>
+                <IonCardSubtitle>Card 4</IonCardSubtitle>
+                <IonCardTitle>Feature 4</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>This is the feature.</IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+      </IonGrid> */}
+      <IonContent>
+        <LeafletMap />
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default UserDashboard;
